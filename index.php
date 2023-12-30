8f9162184bec52412339a54eb235281a59bfaa52
Adam Bawany <bawany.adam@gmail.com>
2023-12-30 15:11:22 -0800

    - Updated to retrieve text posts as well. - Still missing profile image (probably won't happen)

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
+<?php
+// Retrieve the URL from the 'q' GET parameter
+$url = isset($_GET['q']) ? $_GET['q'] : '';
+?>
 <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2, shrink-to-fit=no"/>
 <meta property="og:type" content="article"/>
-<link href="" type="application/activity+json"/>
-<meta name="twitter:card" content="summary_large_image"/>
+<meta property="og:site_name" content="Bluesky"/>
 <?php
-// Retrieve the URL from the 'p' GET parameter
-$url = isset($_GET['q']) ? $_GET['q'] : '';
+
 
 // Validate the URL
 if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
@@ -34,7 +36,7 @@ $tags = [
     'og:title' => "//meta[@property='og:title']",
     'og:description' => "//meta[@property='og:description']",
     'og:url' => "//meta[@property='og:url']",
-    'og:image' => "//meta[@property='og:image']"
+    'twitter:card' => "//meta[@name='twitter:card']",
 ];
 
 // Extract and echo the HTML tags
@@ -42,12 +44,45 @@ foreach ($tags as $tag => $query) {
     $elements = $xpath->query($query);
     if ($elements->length > 0) {
         $element = $elements->item(0);
+
+        if ($tag === 'twitter:card') {
+            $cardValue = $element->getAttribute('content');
+            
+            // Check if twitter:card is not "summary_large_image" and change it to "summary"
+            if ($cardValue !== 'summary_large_image') {
+                $element->setAttribute('content', 'summary');
+            }
+        }
+
+        // Echo the modified or original tag
         echo $dom->saveHTML($element) . "\n";
     } else {
         echo $tag . " tag not found.\n";
     }
 }
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
+// Retrieve og:image only when twitter:card is "summary_large_image"
+if (isset($cardValue) && $cardValue === 'summary_large_image') {
+    $ogImageQuery = "//meta[@property='og:image']";
+    $ogImageElements = $xpath->query($ogImageQuery);
+    if ($ogImageElements->length > 0) {
+        $ogImageElement = $ogImageElements->item(0);
+        echo $dom->saveHTML($ogImageElement) . "\n";
+    } else {
+        echo "og:image tag not found.\n";
+    }
+}
+
 ?>
+<link href="<?php echo "$url"; ?>" type="application/activity+json"/>
+<link rel="canonical" href="<?php echo "$url"; ?>"/>
+ <script type="text/javascript">
+    window.open("<?php echo "$url"; ?>", "_self");
+    window.open("", "_self");
+    setTimeout("window.close()",1000) 
+
+
+  
+
+</script>
+
